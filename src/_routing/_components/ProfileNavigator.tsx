bfa445b01865100a342e192e3e52c6611ad15ce9
Sam Van Hoey <sam.vanhoey@icapps.com>
2022-11-21 11:01:13 +0100

    put header in const

--- src/_routing/_components/ProfileNavigator.tsx
+++ src/_routing/_components/ProfileNavigator.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { theme } from '../../_styles/theme';
+import { generalStyles } from '../../_styles/constants';
 import i18n from '../../_translations/i18n';
 import About from '../../about/About';
 import Profile from '../../profile/Profile';
@@ -13,19 +13,21 @@ const Stack = createNativeStackNavigator<TProfileParams>();
 
 export const ProfileNavigator: FC = () => {
   return (
-    <Stack.Navigator initialRouteName="Profile" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        headerShown: false,
+        ...generalStyles.navigationHeader,
+      }}
+    >
       <Stack.Screen component={Profile} name="Profile" />
       <Stack.Screen
         component={About}
         name="About"
         options={{
           headerBackTitle: i18n.t('PROFILE.ABOUT.BACK_TITLE'),
-          headerBackTitleStyle: { fontFamily: 'Poppins-SemiBold' },
           headerShown: true,
-          headerStyle: { backgroundColor: theme.colors.primary },
-          headerTintColor: theme.colors.white,
           headerTitle: i18n.t('PROFILE.ABOUT.HEADER_TITLE'),
-          headerTitleStyle: { fontFamily: 'Poppins-SemiBold' },
         }}
       />
     </Stack.Navigator>
---
+++ src/_styles/constants.ts
@@ -0,0 +1,10 @@
+import { theme } from './theme';
+
+export const generalStyles = {
+  navigationHeader: {
+    headerBackTitleStyle: { fontFamily: 'Poppins-SemiBold' },
+    headerStyle: { backgroundColor: theme.colors.primary },
+    headerTintColor: theme.colors.white,
+    headerTitleStyle: { fontFamily: 'Poppins-SemiBold' },
+  },
+};
