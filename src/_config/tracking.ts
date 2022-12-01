4258603f3ab86e8eeb13ce51f4e2a8a2762e1265
hstandaert <helena.standaert@icapps.com>
2022-12-01 15:26:20 +0100

    UIT-26: enable tracking

--- src/_config/tracking.ts
+++ src/_config/tracking.ts
@@ -2,6 +2,6 @@ export const TrackingConfig = {
   appEnvironmentSchema: 'iglu:be.general/app_env/jsonschema/1-0-0',
   appId: 'uitpas-mobile-app',
   endpoint: 'sneeuwploeg.uitpas.be',
-  isEnabled: false, // Might change in the future
+  isEnabled: true,
   passHolderSchema: 'iglu:be.uitpas-mobile/passholder/jsonschema/1-0-0',
 };
