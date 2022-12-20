7515b2d2305b350da67fa21097fe83208b2d8574
hstandaert <helena.standaert@icapps.com>
2022-12-20 09:33:40 +0100

    UIT-66: Add Sentry logging

--- src/_utils/logger.ts
+++ src/_utils/logger.ts
@@ -1,22 +1,42 @@
 import { Config } from 'react-native-config';
-import { logger } from 'react-native-logs';
+import { configLoggerType, logger, sentryTransport } from 'react-native-logs';
+import * as Sentry from '@sentry/react-native';
 
-import { TConfigEnvironment } from '../_config/environments';
-
-export const log = logger.createLogger({
-  enabled: TConfigEnvironment !== 'production',
+let config: configLoggerType = {
   levels: {
     debug: 0,
     error: 3,
     info: 1,
     warn: 2,
   },
-  severity: Config.REACT_NATIVE_APP_LOGGING_LEVEL || 'info',
-  transportOptions: {
-    colors: {
-      error: 'redBright',
-      info: 'blueBright',
-      warn: 'yellowBright',
+};
+
+// When running with the RN packager, we don't want to transport logs to Sentry
+if (__DEV__) {
+  config = {
+    ...config,
+    severity: Config.REACT_NATIVE_APP_LOGGING_LEVEL || 'info',
+    transportOptions: {
+      colors: {
+        error: 'redBright',
+        info: 'blueBright',
+        warn: 'yellowBright',
+      },
     },
-  },
-});
+  };
+} else {
+  Sentry.init({
+    dsn: Config.SENTRY_DSN,
+  });
+
+  config = {
+    ...config,
+    severity: Config.REACT_NATIVE_APP_LOGGING_LEVEL || 'error',
+    transport: sentryTransport,
+    transportOptions: {
+      SENTRY: Sentry,
+    },
+  };
+}
+
+export const log = logger.createLogger<'debug' | 'info' | 'warn' | 'error'>(config);
