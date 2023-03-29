a04e69e4014646a7669c9343b68b378949ef0703
Jasper Vercammen <jasper.vercammen@icapps.com>
2023-03-29 19:55:52 +0200

    feat: added get rewards service

---
+++ src/shop/_models/reward.ts
@@ -0,0 +1,68 @@
+import { TCity } from '../../profile/_models';
+
+export type TRewardCategory = 'Eten en drinken' | 'Doen' | 'Gadget of item' | 'Goede doel';
+export type TRewardStatus = 'ACTIVE' | 'INACTIVE' | 'DELETED';
+export type TRewardType = 'POINTS' | 'WELCOME';
+
+export type TCardSystem = {
+  allowsCardlessRegistration: boolean;
+  branding?: {
+    logo?: string;
+    primaryColor?: string;
+    secondaryColor?: string;
+  };
+  cities: TCity[];
+  id: number;
+  links?: {
+    website?: string;
+  };
+  name: string;
+  permanent: boolean;
+};
+
+type TPeriod = {
+  begin: string;
+  end?: string;
+};
+
+type TRedeemConstrainPeriod = 'ABSOLUTE' | 'DAY' | 'WEEK' | 'MONTH' | 'QUARTER' | 'YEAR';
+
+export type TReward = {
+  allCardSystems: boolean;
+  applicableCardSystems: TCardSystem[];
+  categories: TRewardCategory[];
+  featured: boolean;
+  forKids: boolean;
+  grantingPeriod: TPeriod;
+  id: string;
+  inSpotlight: boolean;
+  maxAvailableUnits: number;
+  moreInfoURL: string;
+  online: boolean;
+  organizers: {
+    id: string;
+    name: string;
+  };
+  owningCardSystem: TCardSystem;
+  pictures: string[];
+  points: number;
+  practicalInfo: string;
+  promotionalDescription: string;
+  publicationPeriod: TPeriod;
+  redeemConstraint: {
+    period: TRedeemConstrainPeriod;
+    volume: number;
+  };
+  redeemPeriod: TPeriod;
+  sport: boolean;
+  status: TRewardStatus;
+  title: string;
+  type: TRewardType;
+  unitsTaken: number;
+};
+
+export type TRewardsResponse = {
+  facet: unknown;
+  member: TReward[];
+  totalItems: number;
+};
---
+++ src/shop/_queries/useGetRewards.ts
@@ -0,0 +1,82 @@
+import { usePubliqApi } from '../../_hooks/usePubliqApi';
+import { TApiError } from '../../_http';
+import { useGetMe } from '../../profile/_queries/useGetMe';
+import { TRewardCategory, TRewardsResponse, TRewardType } from '../_models/reward';
+
+type TFilterRewardCategory = TRewardCategory | 'laatste kans';
+type TFilterRewardSections = 'online' | 'in de kijker' | 'populair regio' | 'populair' | 'stad voordelen' | 'sport';
+
+export function useGetRewards({
+  category,
+  type = 'POINTS',
+  section,
+  itemsPerPage = 20,
+  extraParams = {},
+}: {
+  category?: TFilterRewardCategory;
+  extraParams?: Record<string, string | boolean | number>;
+  itemsPerPage?: number;
+  section?: TFilterRewardSections;
+  type?: TRewardType;
+} = {}) {
+  const api = usePubliqApi();
+  const { data: user } = useGetMe();
+
+  // get params, already add the default sorting
+  const params: Record<string, string | boolean | number> = {
+    ['sort[redeemCount]']: 'desc',
+  };
+
+  let owningCardSystemIdParam = `?${user.cardSystemMemberships
+    .map(membership => `owningCardSytemId=${membership.cardSystem.id}`)
+    .join('&')}`;
+
+  // add type
+  if (type) {
+    params.type = type;
+  }
+  // add category
+  if (category === 'laatste kans') {
+    params.lastChance = true;
+  } else if (category) {
+    params.categories = category;
+    delete params['sort[redeemCount]'];
+  }
+
+  // filter for a section
+  switch (section) {
+    case 'online':
+      params.online = true;
+      delete params['sort[redeemCount]'];
+      break;
+    case 'in de kijker':
+      params.featured = true;
+      break;
+    case 'populair':
+      owningCardSystemIdParam = '';
+      // Remove the owningCardSystemId param so we get popular rewards for every region
+      delete params.owningCardSystemId;
+      break;
+    case 'populair regio':
+      // No extra params needed
+      break;
+    case 'stad voordelen':
+      params.organizerPostalCode = user.address.postalCode;
+      break;
+    case 'sport':
+      params.sport = true;
+      break;
+  }
+
+  return api.getInfinite<TRewardsResponse>(
+    ['rewards', JSON.stringify(params), owningCardSystemIdParam],
+    `/rewards${owningCardSystemIdParam}`,
+    {
+      itemsPerPage,
+      onError: (_error: TApiError) => {
+        // TODO: Handle error
+      },
+      params: { ...params, ...extraParams },
+    },
+  );
+}
