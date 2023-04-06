0ee2eecc424c2b585250a39b0eea2529bdf5f66d
Jasper Vercammen <jasper.vercammen@icapps.com>
2023-04-06 15:36:23 +0200

    fix: change horizontal/vvertical order

--- src/shop/Shop.tsx
+++ src/shop/Shop.tsx
@@ -11,14 +11,14 @@ const Shop = () => {
   return (
     <SafeAreaView edges={['left', 'right']} isScrollable>
       <CategoryFilters />
-      <RewardsSection filter="online" title={t('SHOP.SECTIONS.ONLINE')} />
-      <RewardsSection filter="in de kijker" horizontal title={t('SHOP.SECTIONS.HIGHLIGHTED')} />
-      <RewardsSection category="laatste kans" title={t('SHOP.SECTIONS.LAST_CHANCE')} />
-      <RewardsSection filter="populair regio" horizontal title={t('SHOP.SECTIONS.POPULAR_REGION')} />
-      <RewardsSection filter="stad voordelen" title={t('SHOP.SECTIONS.CITY')} />
-      <RewardsSection filter="populair" horizontal title={t('SHOP.SECTIONS.POPULAR')} />
-      <RewardsSection filter="sport" title={t('SHOP.SECTIONS.SPORTIVE')} />
-      <RewardsSection category="Goede doel" horizontal title={t('SHOP.SECTIONS.CHARITY')} />
+      <RewardsSection filter="online" horizontal title={t('SHOP.SECTIONS.ONLINE')} />
+      <RewardsSection filter="in de kijker" title={t('SHOP.SECTIONS.HIGHLIGHTED')} />
+      <RewardsSection category="laatste kans" horizontal title={t('SHOP.SECTIONS.LAST_CHANCE')} />
+      <RewardsSection filter="populair regio" title={t('SHOP.SECTIONS.POPULAR_REGION')} />
+      <RewardsSection filter="stad voordelen" horizontal title={t('SHOP.SECTIONS.CITY')} />
+      <RewardsSection filter="populair" title={t('SHOP.SECTIONS.POPULAR')} />
+      <RewardsSection filter="sport" horizontal title={t('SHOP.SECTIONS.SPORTIVE')} />
+      <RewardsSection category="Goede doel" title={t('SHOP.SECTIONS.CHARITY')} />
     </SafeAreaView>
   );
 };
