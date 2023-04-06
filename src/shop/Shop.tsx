929145e599a2b8207ff5ff77db93fcb89246e592
Jasper Vercammen <jasper.vercammen@icapps.com>
2023-04-06 12:59:42 +0200

    feat: added correct lanes

--- src/shop/Shop.tsx
+++ src/shop/Shop.tsx
@@ -11,11 +11,14 @@ const Shop = () => {
   return (
     <SafeAreaView edges={['left', 'right']} isScrollable>
       <CategoryFilters />
-      <RewardsSection filter="populair" horizontal title={t('SHOP.SECTIONS.POPULAR')} />
-      <RewardsSection filter="in de kijker" title={t('SHOP.SECTIONS.HIGHLIGHTED')} />
-      <RewardsSection filter="populair regio" horizontal title={t('SHOP.SECTIONS.POPULAR_REGION')} />
       <RewardsSection filter="online" title={t('SHOP.SECTIONS.ONLINE')} />
-      <RewardsSection filter="sport" horizontal title={t('SHOP.SECTIONS.SPORT')} />
+      <RewardsSection filter="in de kijker" horizontal title={t('SHOP.SECTIONS.HIGHLIGHTED')} />
+      <RewardsSection category="laatste kans" title={t('SHOP.SECTIONS.LAST_CHANCE')} />
+      <RewardsSection filter="populair regio" horizontal title={t('SHOP.SECTIONS.POPULAR_REGION')} />
+      <RewardsSection filter="stad voordelen" title={t('SHOP.SECTIONS.CITY')} />
+      <RewardsSection filter="populair" horizontal title={t('SHOP.SECTIONS.POPULAR')} />
+      <RewardsSection filter="sport" title={t('SHOP.SECTIONS.SPORTIVE')} />
+      <RewardsSection category="Goede doel" horizontal title={t('SHOP.SECTIONS.CHARITY')} />
     </SafeAreaView>
   );
 };
