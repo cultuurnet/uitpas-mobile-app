97097da046307584d6deb0c3626c326bd6e6b0a8
Jasper Vercammen <jasper.vercammen@icapps.com>
2023-04-27 06:56:41 +0200

    fix: crash after changing capital letter for textContainter

--- src/_components/trans/Trans.tsx
+++ src/_components/trans/Trans.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Trans as TransI18n, TransProps } from "react-i18next";
-import { TFuncKey } from 'i18next';
 
 import Typography from '../typography/Typography';
 
 
-const Trans = <
-  K extends TFuncKey<N, TKPrefix> extends infer A ? A : never,
-  N extends Namespace = DefaultNamespace,
-  TKPrefix extends KeyPrefix<N> = undefined,
-  E = React.HTMLProps<HTMLDivElement>
->(props: TransProps<K, N, TKPrefix, E>) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Trans = (props: TransProps<any>) => {
   return (
     <TransI18n
       components={{
