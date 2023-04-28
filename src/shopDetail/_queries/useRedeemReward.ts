b0035da26cbbeae227c5ea99754535dddce6cada
Jasper Vercammen <vercammenjasper@gmail.com>
2023-04-28 15:55:21 +0200

    Update src/shopDetail/_queries/useRedeemReward.ts
    Co-authored-by: samvanhoeyicapps <90620650+samvanhoeyicapps@users.noreply.github.com>

--- src/shopDetail/_queries/useRedeemReward.ts
+++ src/shopDetail/_queries/useRedeemReward.ts
@@ -7,7 +7,7 @@ export function useRedeemReward(options: TPostOptions<TRedeemedReward>) {
   return api.post<TRedeemedReward>(['redeem-rewards'], '/rewards/redeemed', {
     ...options,
     onSuccess: (data, variables, context) => {
-      // Reset me callm so we will fetch the userpoints again
+      // Reset me call so we will fetch the userpoints again
       queryClient.invalidateQueries(['me']);
       options.onSuccess(data, variables, context);
     },
