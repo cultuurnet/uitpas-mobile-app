4a81b42df3c929d6ae528b540277e01d525d1594
Jasper Vercammen <jasper.vercammen@icapps.com>
2023-05-02 12:02:23 +0200

    fix: no text

--- src/_components/navigation/NavigationBar.tsx
+++ src/_components/navigation/NavigationBar.tsx
@@ -17,8 +17,8 @@ const NavigationBar: FC = () => {
           <Circle cx="50%" cy="0" fill="#000" r="39" />
         </Mask>{/* A mask 1 px smaller than the real mask, so we create a border */}
       </Defs>
-      {Platform.OS === 'android' && <Rect fill={theme.palette.neutral['100']} height="100%" mask="url(#cutout-border)" width="100%" x={0} y={0} />} {/* Only on Android, this will represent the border of the navigationbar */}
-      <Rect fill={theme.palette.neutral['0']} height="100%" mask="url(#cutout)" width="100%" x={0} y={Platform.OS === 'android' ? 1 : 0} /> {/* offset of 1, so it looks like the other bar is the border */}
+      {Platform.OS === 'android' && <Rect fill={theme.palette.neutral['100']} height="100%" mask="url(#cutout-border)" width="100%" x={0} y={0} />}{/* Only on Android, this will represent the border of the navigationbar */}
+      <Rect fill={theme.palette.neutral['0']} height="100%" mask="url(#cutout)" width="100%" x={0} y={Platform.OS === 'android' ? 1 : 0} />{/* offset of 1, so it looks like the other bar is the border */}
     </Svg>
   );
 };
