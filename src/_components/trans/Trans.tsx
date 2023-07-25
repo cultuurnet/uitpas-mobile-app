02caa5e89f80a45108b7d2c1ba3e4887471f9afb
Sam Van Hoey <sam.vanhoey@icapps.com>
2023-07-25 09:56:34 +0200

    added translations with buttons

--- src/_components/trans/Trans.tsx
+++ src/_components/trans/Trans.tsx
@@ -5,14 +5,14 @@ import Typography, { TTypographyProps } from '../typography/Typography';
 import * as Styled from './style';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type TProps = TTypographyProps & Omit<TransProps<any>, 'size'> & { buttonOnPress?: () => void };
+type TProps = TTypographyProps & Omit<TransProps<any>, 'size'> & { onButtonPress?: () => void };
 
 const Trans = (props: TProps) => {
   return (
     <TransI18n
       components={{
         bold: <Typography fontStyle="bold" size={props.size} />,
-        button: <Styled.UnderlinedLinkText onPress={props.buttonOnPress} size={props.size} />,
+        button: <Styled.UnderlinedLinkText onPress={props.onButtonPress} size={props.size} />,
       }}
       parent={Typography}
       {...props}
