335c19885c210ef2b4476fdb12ce9ad0d803f9ad
Sam Van Hoey <sam.vanhoey@icapps.com>
2023-11-17 11:17:16 +0100

    move familyOverview to index

--- src/onboarding/family/index.ts
+++ src/onboarding/family/index.ts
@@ -1,3 +1,4 @@
 export * from './FamilyOnboarding';
 export * from './addFamilyMember/AddFamilyMember';
 export * from './familyInformation/FamilyInformation';
+export * from './familyOverview/FamilyOverview';
