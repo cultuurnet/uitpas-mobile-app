e98945a9e6a0707d04d62556a905456f8420f12c
Chennara Nhes <chennara.nhes@gmail.com>
2023-11-30 10:12:45 +0100

    refactor: remove UserPoints component

--- src/_components/userPoints/UserPoints.tsx
+++
@@ -1,9 +0,0 @@
-import { useFamilyComposition } from '../../profile/family/hooks';
-import { FamilyUserPoints } from './familyUserPoints/FamilyUserPoints';
-import { SingleUserPoints } from './singleUserPoints/SingleUserPoints';
-
-const UserPoints = () => {
-  return useFamilyComposition({ FamilyComponent: FamilyUserPoints, SingleComponent: SingleUserPoints });
-};
-
-export default UserPoints;
