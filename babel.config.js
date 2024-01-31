a76fa75e3221fbcd431fbb3d077ec0d688ef3a7d
Sam Van Hoey <sam.vanhoey@icapps.com>
2024-01-31 17:02:53 +0100

    feat: UIT-232 wip for vision camera 3 upgrade

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   plugins: [
-    ['react-native-reanimated/plugin', { // reanimated must be the last package
-      globals: ['__scanCodes'],
-    }]
+    ['react-native-worklets-core/plugin'],
+    [
+      'react-native-reanimated/plugin',
+      {
+        // reanimated must be the last package
+        globals: ['__scanCodes'],
+      },
+    ],
   ],
   presets: ['module:metro-react-native-babel-preset'],
 };
--- src/scan/camera/Camera.tsx
+++ src/scan/camera/Camera.tsx
@@ -1,30 +1,25 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
-import { runOnJS } from 'react-native-reanimated';
-import { Camera as VisionCamera, Frame, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
+import {
+  Camera as VisionCamera,
+  runAtTargetFps,
+  useCameraDevices,
+  useCameraFormat,
+  useCodeScanner,
+} from 'react-native-vision-camera';
+import { CameraHighlights, useBarcodeScanner } from '@mgcrea/vision-camera-barcode-scanner';
 import { useFocusEffect } from '@react-navigation/native';
-import { Barcode, BarcodeFormat, scanBarcodes } from 'vision-camera-code-scanner';
 
 import { Analytics, FocusAwareStatusBar, Spinner } from '../../_components';
 import { useTracking } from '../../_context';
-import { TApiError } from '../../_http';
 import { TMainNavigationProp } from '../../_routing/_components/TRootStackParamList';
 import { theme } from '../../_styles/theme';
-import { log, TRACKING_URL_REGEX } from '../../_utils';
 import { useHasFamilyMembers } from '../../onboarding/family/_queries';
 import { useGetMe } from '../../profile/_queries/useGetMe';
 import { useCameraPermission } from '../_hooks';
-import { TOverlayDimensions, useOverlayDimensions } from '../_hooks/useOverlayDimensions';
 import { useCheckin } from '../_queries/useCheckin';
-import { isInRange } from '../_util/isInRange';
 import CameraSettings from '../cameraSettings/CameraSettings';
-import CameraOverlay from './CameraOverlay';
-
-const overlaySettings: TOverlayDimensions = {
-  cornerLength: 20,
-  padding: 75,
-  strokeWidth: 4,
-};
+import { NewCameraOverlay } from './NewCameraOverlay';
 
 type TProps = {
   navigation: TMainNavigationProp<'Camera'>;
@@ -32,98 +27,95 @@ type TProps = {
 
 const Camera = ({ navigation }: TProps) => {
   const [isActive, setIsActive] = useState(true);
-  const { trackSelfDescribingEvent } = useTracking();
   const { data: me } = useGetMe();
-  const { back: device } = useCameraDevices();
   const { hasCameraPermission } = useCameraPermission();
-  const [overlayDimensions, setOverlayDimensions] = useState({ height: 0, width: 0 });
-  const overlay = useOverlayDimensions(overlayDimensions, overlaySettings);
+  // const [overlayDimensions, setOverlayDimensions] = useState({ height: 0, width: 0 });
+  // const overlay = useOverlayDimensions(overlayDimensions, overlaySettings);
   const { mutateAsync: checkin, isLoading } = useCheckin();
   const { data: hasFamilyMembers } = useHasFamilyMembers();
-  const frameProcessor = useFrameProcessor(
-    frame => {
-      'worklet';
-      const barcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE]);
-      if (isActive && !isLoading && barcodes.length > 0) {
-        runOnJS(onBarCodeDetected)(barcodes[0], frame);
-      }
+
+  useEffect(() => {
+    const runEffect = async () => {
+      setTimeout(() => {
+        setIsActive(true);
+      }, 1000);
+    };
+    runEffect();
+  }, []);
+
+  const codeScanner = useCodeScanner({
+    regionOfInterest
+    codeTypes: ['qr'],
+    onCodeScanned: scannedCodes => {
+      runAtTargetFps(5, () => {
+        // console.log(`Scanned ${scannedCodes.length} codes!`);
+        // setCodes((codes: Code[]) => [...codes, ...scannedCodes]);
+        console.log(JSON.stringify(scannedCodes, null, 2));
+      });
     },
-    [overlayDimensions, isActive, isLoading],
-  );
+  });
 
-  useFocusEffect(
-    useCallback(() => {
-      setIsActive(true);
-      return () => setIsActive(false);
-    }, []),
-  );
-
-  function handleLayoutChange({ nativeEvent: { layout } }: LayoutChangeEvent) {
-    const { width, height } = layout;
-    setOverlayDimensions({ height, width });
-  }
-
-  async function onBarCodeDetected(barcode: Barcode, frame: Frame) {
-    const frameWidth = frame.width > frame.height ? frame.height : frame.width;
-    const frameHeight = frame.width > frame.height ? frame.width : frame.height;
-
-    if (isInRange(barcode, overlay.regionDefinition, [frameWidth, frameHeight])) {
-      try {
-        setIsActive(false);
-        const response = await checkin({
-          body: { checkinCode: barcode.displayValue },
-        });
-        trackSelfDescribingEvent(
-          'successMessage',
-          { message: 'points-saved-success' },
-          { up_action: { name: 'save-points', points: response.addedPoints, target: 'self', target_ph_id: me?.id } },
-        );
-        navigation.navigate('ScanSuccess', { ...response, checkinCode: barcode.displayValue });
-      } catch (error) {
-        const { endUserMessage } = error as TApiError;
-        trackSelfDescribingEvent(
-          'errorMessage',
-          { message: error.type.replace(TRACKING_URL_REGEX, '') },
-          { up_action: { name: 'save-points', points: undefined, target: 'self', target_ph_id: me?.id } },
-        );
-        navigation.navigate('Error', {
-          checkinCode: barcode.displayValue,
-          gotoAfterClose: ['MainNavigator', 'Profile'],
-          message: endUserMessage?.nl,
-          showFamilyScan: hasFamilyMembers,
-        });
-        log.error(error);
-      }
-    }
-  }
+  const devices = useCameraDevices();
+  const device = devices.find(({ position }) => position === 'back');
+  const format = useCameraFormat(device, [{ videoResolution: { height: 1080, width: 1920 } }]);
+  // function handleLayoutChange({ nativeEvent: { layout } }: LayoutChangeEvent) {
+  //   const { width, height } = layout;
+  //   // setOverlayDimensions({ height, width });
+  // }
 
   if (!hasCameraPermission) {
     return <CameraSettings />;
   }
-
-  if (device == null) {
+  console.log('device', device);
+  console.log('format', format);
+  if (!device || !format) {
     return <Spinner />;
   }
 
   return (
     <>
       <Analytics screenName="Camera" />
-      <View onLayout={handleLayoutChange} style={StyleSheet.absoluteFill}>
+      <View style={StyleSheet.absoluteFill}>
         <FocusAwareStatusBar backgroundColor={theme.palette.neutral['900']} barStyle="light-content" />
-        {overlayDimensions.width !== 0 && (
-          <VisionCamera
-            device={device}
-            frameProcessor={frameProcessor}
-            frameProcessorFps={5}
-            isActive={isActive}
-            style={overlayDimensions}
-          />
-        )}
-
-        <CameraOverlay config={overlay} isLoading={isLoading} settings={overlaySettings} />
+        <VisionCamera codeScanner={codeScanner} device={device} isActive={isActive} style={StyleSheet.absoluteFill} />
+        <NewCameraOverlay />
       </View>
     </>
   );
 };
 
 export default Camera;
+
+// async function onBarCodeDetected(barcode: Barcode, frame: Frame) {
+//   const frameWidth = frame.width > frame.height ? frame.height : frame.width;
+//   const frameHeight = frame.width > frame.height ? frame.width : frame.height;
+
+//   if (isInRange(barcode, overlay.regionDefinition, [frameWidth, frameHeight])) {
+//     try {
+//       setIsActive(false);
+//       const response = await checkin({
+//         body: { checkinCode: barcode.displayValue },
+//       });
+//       trackSelfDescribingEvent(
+//         'successMessage',
+//         { message: 'points-saved-success' },
+//         { up_action: { name: 'save-points', points: response.addedPoints, target: 'self', target_ph_id: me?.id } },
+//       );
+//       navigation.navigate('ScanSuccess', { ...response, checkinCode: barcode.displayValue });
+//     } catch (error) {
+//       const { endUserMessage } = error as TApiError;
+//       trackSelfDescribingEvent(
+//         'errorMessage',
+//         { message: error.type.replace(TRACKING_URL_REGEX, '') },
+//         { up_action: { name: 'save-points', points: undefined, target: 'self', target_ph_id: me?.id } },
+//       );
+//       navigation.navigate('Error', {
+//         checkinCode: barcode.displayValue,
+//         gotoAfterClose: ['MainNavigator', 'Profile'],
+//         message: endUserMessage?.nl,
+//         showFamilyScan: hasFamilyMembers,
+//       });
+//       log.error(error);
+//     }
+//   }
+// }
