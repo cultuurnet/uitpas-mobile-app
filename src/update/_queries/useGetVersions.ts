ab6f786dd7174cd1096f035016c15ad6386992b1
Sam Van Hoey <sam@MacBook-Pro-van-Sam.local>
2024-05-07 15:10:41 +0200

    fix: use mock versions endpoint

--- src/update/_queries/useGetVersions.ts
+++ src/update/_queries/useGetVersions.ts
@@ -1,19 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { ConfigUrl } from '../../_config';
 import { HttpClient } from '../../_http';
 import { TVersion } from '../_models';
 import { checkVersion } from '../_util/checkVersion';
 
 function getVersions(): Promise<TVersion> {
-  return HttpClient.get<TVersion>(ConfigUrl.version);
+  return HttpClient.get<TVersion>('https://663a23451ae792804bee3482.mockapi.io/versions');
 }
 
 export function useGetVersions() {
-  const { data: versions } = useQuery<TVersion, unknown>(['versions'], () => getVersions(), {
+  const { data: versions } = useQuery<TVersion[], unknown>(['versions'], () => getVersions(), {
     cacheTime: 0,
     networkMode: 'online',
-    refetchOnWindowFocus: true,
+    refetchOnWindowFocus: 'always',
   });
-  return versions ? checkVersion(versions) : undefined;
+  return versions[0] ? checkVersion(versions[0]) : undefined;
 }
