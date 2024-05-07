6d7bcd6cd8f32893b88509af424791e18a8d4a1a
samvanhoey <sam.vanhoey@icapps.com>
2024-05-07 20:00:27 +0200

    fix: UIT-188 refetch always

--- src/update/_queries/useGetVersions.ts
+++ src/update/_queries/useGetVersions.ts
@@ -13,7 +13,7 @@ export function useGetVersions() {
   const { data: versions } = useQuery<TVersion, unknown>(['versions'], () => getVersions(), {
     cacheTime: 0,
     networkMode: 'online',
-    refetchOnWindowFocus: true,
+    refetchOnWindowFocus: 'always',
   });
   return versions ? checkVersion(versions) : undefined;
 }
