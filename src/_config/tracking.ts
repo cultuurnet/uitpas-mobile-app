d1ffcee43534cb50c3b3ce0fc21849e80dbb068c
samvanhoey <sam.vanhoey@icapps.com>
2025-04-02 10:44:30 +0200

    🔧 Add TRACKING_HOST env for variable snowplow tracking

--- src/_config/tracking.ts
+++ src/_config/tracking.ts
@@ -1,9 +1,11 @@
+import { Config } from 'react-native-config';
+
 import { TTrackingData, TTrackingEvents } from '../_models';
 
 export const TrackingConfig = {
   appEnvironmentSchema: 'iglu:be.general/app_env/jsonschema/1-0-0',
   appId: 'uitpas-mobile-app',
-  endpoint: 'sneeuwploeg.uitpas.be',
+  endpoint: Config.TRACKING_HOST,
   familySchema: 'iglu:be.uitpas-mobile/family/jsonschema/1-0-0',
   isEnabled: !__DEV__,
   passHolderSchema: 'iglu:be.uitpas-mobile/passholder/jsonschema/1-0-0',
