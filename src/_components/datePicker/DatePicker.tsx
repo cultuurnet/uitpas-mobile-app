f5b5065e961ed209170c9122d7f583dfbc28a10a
Helena Standaert <helena.standaert@icapps.com>
2025-11-18 13:21:52 +0100

    UIT-270: use @react-native-community/datetimepicker

--- src/_components/datePicker/DatePicker.tsx
+++ src/_components/datePicker/DatePicker.tsx
@@ -1,9 +1,13 @@
 import { ReactElement, useState } from 'react';
+import { Platform } from 'react-native';
 import { useTranslation } from 'react-i18next';
-import RnDatePicker from 'react-native-date-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { format } from 'date-fns';
 
 import FakeTextInput from '../textInput/fakeTextInput/FakeTextInput';
+import BlurredModal from '../blurredModal/BlurredModal';
+import Button from '../button/Button';
+import * as Styled from './style';
 
 type TProps = {
   date?: Date;
@@ -17,6 +21,7 @@ const DatePicker = ({ date, onSelectDate, ...textInputProps }: TProps) => {
   const { t } = useTranslation();
 
   const [isOpen, setIsOpen] = useState(false);
+  const [tempDate, setTempDate] = useState(date ?? new Date());
 
   const handleOpen = () => {
     setIsOpen(true);
@@ -26,26 +31,43 @@ const DatePicker = ({ date, onSelectDate, ...textInputProps }: TProps) => {
     setIsOpen(false);
   };
 
-  const handleConfirm = (nextDate: Date) => {
-    onSelectDate(nextDate);
+  const handleChange = (event: any, selectedDate?: Date) => {
+    if (Platform.OS === 'android') {
+      setIsOpen(false);
+      if (event.type === 'set' && selectedDate) {
+        onSelectDate(selectedDate);
+      }
+    } else {
+      if (selectedDate) {
+        setTempDate(selectedDate);
+      }
+    }
+  };
+
+  const handleConfirm = () => {
+    onSelectDate(tempDate);
     handleClose();
   };
 
   return (
     <>
       <FakeTextInput {...textInputProps} onPress={handleOpen} value={date ? format(date, 'dd/MM/yyyy') : ''} />
-      <RnDatePicker
-        cancelText={t('DATE_PICKER.CANCEL')}
-        confirmText={t('DATE_PICKER.CONFIRM')}
-        date={date ?? new Date()}
-        locale="nl"
-        modal
-        mode="date"
-        onCancel={handleClose}
-        onConfirm={handleConfirm}
-        open={isOpen}
-        title={null}
-      />
+      <BlurredModal isVisible={isOpen} toggleIsVisible={handleClose}>
+        <Styled.ModalContent>
+          <DateTimePicker
+            value={tempDate}
+            mode="date"
+            display={Platform.OS === 'ios' ? 'inline' : 'default'}
+            onChange={handleChange}
+            locale="nl"
+          />
+          {Platform.OS === 'ios' && (
+            <Styled.ButtonContainer>
+              <Button onPress={handleConfirm} label={t('DATE_PICKER.CONFIRM')} />
+            </Styled.ButtonContainer>
+          )}
+        </Styled.ModalContent>
+      </BlurredModal>
     </>
   );
 };
---
+++ src/_components/datePicker/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const ModalContent = styled.View`
+  background-color: ${({ theme }) => theme.palette.neutral['0']};
+  align-items: center;
+`;
+
+export const ButtonContainer = styled.View`
+  margin-top: 16px;
+  width: 100%;
+`;
