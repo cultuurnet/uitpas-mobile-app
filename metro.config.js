00b266306d02ad079b60f50ce3d6a751a3168248
Wouter Landuydt <wouter.landuydt@gmail.com>
2026-09-24 11:32:43 +0200

    fix: upgrade sentry to 8.0

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+
+// const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
+
+module.exports = config;
